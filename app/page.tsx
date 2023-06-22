4dd4339fdc42f1ec6c48ed1a7e2d069a1d4f9a3f
Arghyajit Das <116450956+arghyajitdas22@users.noreply.github.com>
2023-06-22 14:09:42 +0530

    change of dynamic

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,27 @@
+export const dynamic = "auto";
 import Container from "@/app/components/Container";
 import ListingCard from "@/app/components/listings/ListingCard";
 import EmptyState from "@/app/components/EmptyState";
 
-import getListings, { 
-  IListingsParams
-} from "@/app/actions/getListings";
+import getListings, { IListingsParams } from "@/app/actions/getListings";
 import getCurrentUser from "@/app/actions/getCurrentUser";
 
-
 interface HomeProps {
-  searchParams: IListingsParams
-};
+  searchParams: IListingsParams;
+}
 
 const Home = async ({ searchParams }: HomeProps) => {
   const listings = await getListings(searchParams);
   const currentUser = await getCurrentUser();
 
   if (listings.length === 0) {
-    return (
-        <EmptyState showReset />
-    );
+    return <EmptyState showReset />;
   }
 
   return (
-      <Container>
-        <div 
-          className="
+    <Container>
+      <div
+        className="
             pt-24
             grid 
             grid-cols-1 
@@ -36,17 +32,17 @@ const Home = async ({ searchParams }: HomeProps) => {
             2xl:grid-cols-6
             gap-8
           "
-        >
-          {listings.map((listing: any) => (
-            <ListingCard
-              currentUser={currentUser}
-              key={listing.id}
-              data={listing}
-            />
-          ))}
-        </div>
-      </Container>
-  )
-}
+      >
+        {listings.map((listing: any) => (
+          <ListingCard
+            currentUser={currentUser}
+            key={listing.id}
+            data={listing}
+          />
+        ))}
+      </div>
+    </Container>
+  );
+};
 
 export default Home;
